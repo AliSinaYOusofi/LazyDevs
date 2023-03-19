2db8e96506eb488cf4b2aefeaf7a17e165ed4f96
ShahAli <senayousofiali@gmail.com>
2023-03-20 01:48:32 +0500

    bug not fixed route

--- front-end/components/Login/Login.jsx
+++ front-end/components/Login/Login.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 export default function Login() {
     return (
         <>
-            <img 
+            {/* <img 
                 src="https://storyset.com/illustration/nerd/amico#B9A9BEFF&hide=&hide=complete" 
                 alt="image"
                 className="w-10"
-            />
+            >
+            </img> */}
         </>
     )
 }
