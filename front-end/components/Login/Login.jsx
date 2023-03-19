727343f3871bb8c848fd0dda3e77252dfaab4564
ShahAli <senayousofiali@gmail.com>
2023-03-19 17:11:03 +0500

    login page

---
+++ front-end/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className="w-[90%] mx-auto">
+
+            <img
+                src="https://storyset.com/illustration/nerd/pana#5EB8FFFF&hide=&hide=false"
+                alt="login_illustration"
+            />
+
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
