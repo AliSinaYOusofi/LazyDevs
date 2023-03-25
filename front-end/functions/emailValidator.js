da151aaa58295e175b3c464a088bfe489cb442ef
ShahAli <senayousofiali@gmail.com>
2023-03-25 23:42:26 +0500

    backend-setup

--- front-end/functions/emailValidator.js
+++ front-end/functions/emailValidator.js
@@ -1,1 +1,1 @@
-export const emailValidator = (email) => new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}').test(email.toLowerCase())
+export const emailValidator = (email) => new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}').test(email.toLowerCase() || "")
--- front-end/functions/login_submit/handleLoginSubmit.js
+++ front-end/functions/login_submit/handleLoginSubmit.js
@@ -1,3 +1,15 @@
-export const handleLoginSubmit = async () => {
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import { emailValidator } from '../emailValidator';
+
+
+export const handleLoginSubmit = async (email, password) => {
     
+    if (!email) return toast.error("provide an email address");
+    else if (!password) return toast.error("provide a password");
+    else if (!emailValidator(email)) return toast.error("Invalid email provided");
+    // tha's it for the validation
+    else {
+        // going backend finally
+    }
 }
