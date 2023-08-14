bd576328fd9c0c9887d7473389de3eec3c9bff15
ShahAli <senayousofiali@gmail.com>
2023-08-14 20:52:53 +0500

    food templates

--- front-end/components/templates/Templates.jsx
+++ front-end/components/templates/Templates.jsx
@@ -1,58 +1,18 @@
 "use client"
-import React, { useState, useCallback } from 'react'
-// Import React dependencies.
-// Import the Slate editor factory.
-import { createEditor } from 'slate'
+import React from 'react'
+import TemplateCard from './TemplateCard'
 
-// Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from 'slate-react'
+export default function Templates() {
 
-const initialValue = [
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-]
 
-export default function Templates({text, image}) {
-
-    const [editor] = useState(() => withReact(createEditor()));
-
-    const renderElement = useCallback(props => {
-        switch(props.element.type) {
-            case 'code':
-                return <CodeElement {...props} />
-            default:
-                return <DefaultElement {...props} />
-        }
-    }, [])
-
+    // this should show some cards of some templates used for
+    // creating a blog template
     return (
         <div className="w-full h-full bg-black/20 mt-10">
-            <Slate editor={editor} initialValue={initialValue} >
-                <Editable
-                    renderElement={renderElement}
-                    onKeyDown={ event => {
-                        if (event.key === "&") {
-                            event.preventDefault()
-                            editor.insertText("and")
-                        }
-                    }} 
-                />
-            </Slate>
+            <h1> Food Templates </h1>
+            <div>
+                <TemplateCard image={"https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=600"} content={"The food we eat defines who we are"} title={"The food we want"} />
+            </div>
         </div>
     )
-}
-
-
-const CodeElement = props => {
-    return (
-        <pre {...props.attributes}>
-            <code> {props.children} </code>
-        </pre>
-    )
-}
-
-const DefaultElement = props => {
-    return <p {...props.attributes}> {props.children} </p>
 }
