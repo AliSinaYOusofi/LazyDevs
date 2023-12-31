0d45dbe1fef7e870c8542c770c092f34990fd12a
HajiAli <senayousofiali@gmail.com>
2023-12-31 13:55:08 +0500

    changed: dockerfile

--- front-end/hooks/useSlideAnimation.jsx
+++ front-end/hooks/useSlideAnimation.jsx
@@ -1,28 +1,28 @@
-import { animated, useSpring} from "@react-spring/web";
+// import { animated, useSpring} from "@react-spring/web";
 
-const useSlideAnimation = (fromX, toX) => {
-    const [spring, api] = useSpring(
-        () => ({
-            from: {
-                x: fromX,
-                opacity: 0,
-            },
+// const useSlideAnimation = (fromX, toX) => {
+//     const [spring, api] = useSpring(
+//         () => ({
+//             from: {
+//                 x: fromX,
+//                 opacity: 0,
+//             },
     
-            to: {
-                x: toX,
-                opacity: 1,
-            },
+//             to: {
+//                 x: toX,
+//                 opacity: 1,
+//             },
     
-            config: {
-                damping: 400,
-                clamp: true,
-                tension: 50,
-                friction: 10,
-            },
-        })
-    );
+//             config: {
+//                 damping: 400,
+//                 clamp: true,
+//                 tension: 50,
+//                 friction: 10,
+//             },
+//         })
+//     );
 
-    return [spring, api];
-}
+//     return [spring, api];
+// }
 
-export default useSlideAnimation;
+// export default useSlideAnimation;
